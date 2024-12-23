7b10e3eb6ee0d279d9de202fb5e9e50175e548d7
Xuanhao <shirou-emiya2002@outlook.com>
2024-12-23 22:13:13 +0100

    Fix: should not compare pointer

--- node/quit.go
+++ node/quit.go
@@ -30,7 +30,7 @@ func (node *Node) notifyLeave() {
 	// The method below don't have return value
 	// notify the predecessor to update its successor list
 	predecessor := node.GetPredecessor()
-	if predecessor == &node.info {
+	if predecessor.Identifier == node.info.Identifier {
 		// if the predecessor is the node itself, then we don't need to notify it
 		// because the node itself will be closed soon
 	} else {
@@ -39,7 +39,7 @@ func (node *Node) notifyLeave() {
 
 	// notify the successor to update its predecessor, you can send your predecessor to it
 	successor := node.GetFirstSuccessor()
-	if successor == &node.info {
+	if successor.Identifier == node.info.Identifier {
 		// if the successor is the node itself, then we don't need to notify it
 		// because the node itself will be closed soon
 	} else {
